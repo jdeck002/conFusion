80554f15f5362db9c058b72ac578d7216fc9b163
Unknown <jdeck002@gmail.com>
2018-05-24 18:03:37 -0700

    $http ex complete
    week 4 $http Exercise completed.
    angular-router-ui.min.js updated to remove trailing commas

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -8,8 +8,13 @@ angular.module('confusionApp')
             $scope.filtText = '';
             $scope.showDetails = false;
 
-            $scope.dishes= menuFactory.getDishes();
-
+            $scope.dishes= [];
+            menuFactory.getDishes()
+            .then(
+                function(response) {
+                    $scope.dishes = response.data;
+                }
+            );
                         
             $scope.select = function(setTab) {
                 $scope.tab = setTab;
@@ -70,9 +75,15 @@ angular.module('confusionApp')
 
         .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
 
-            var dish= menuFactory.getDish(parseInt($stateParams.id,10));
             
-            $scope.dish = dish;
+            $scope.dish = {};
+                        menuFactory.getDish(parseInt($stateParams.id,10))
+            .then(
+                function(response){
+                    $scope.dish = response.data;
+                    $scope.showDish=true;
+                }
+            );
             
         }])
 
@@ -96,12 +107,19 @@ angular.module('confusionApp')
         
         .controller('IndexController', ['$scope',  'menuFactory', function($scope, menuFactory) {
 
-            var dish= menuFactory.getDish(0);
 
             var promotion= menuFactory.getPromotion(0);
             
-            $scope.dish = dish;
-            
+            $scope.dish = {};
+
+            menuFactory.getDish(0)
+            .then(
+                function(response){
+                    $scope.dish = response.data;
+                    $scope.showDish = true;
+                }
+            );   
+                     
             $scope.promotion = promotion;
             
         }])
